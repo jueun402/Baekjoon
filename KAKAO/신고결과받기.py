d8fde28768d7a8826557f8acc4813d7cbf282ccd
yjrulrul <56111468+yjrulrul@users.noreply.github.com>
2022-05-05 00:39:52 +0900

    KAKAO UPDATE

--- KAKAO/신고결과받기.py
+++ KAKAO/신고결과받기.py
@@ -25,3 +25,49 @@ def solution(id_list, report, k):
                 result[i] +=1        
 
     return list(result.values())
+
+#----------------------다른 사람 풀이 참고하기 --------------------# 
+# 01. set 활용한 풀이 
+
+id_list = ['muzi', 'frodo', 'apeach', 'neo']
+report = ['muzi frodo', 'apeach frodo', 'frodo neo', 'muzi neo', 'apeach muzi', 'apeach muzi']
+k=2
+
+answer = [0]*len(id_list)
+reports = {x : 0 for x in id_list} # 이렇게 dictionary를 만들 수 있구나!!! 
+
+# 신고 횟수 파악
+# set(report)는 천재...이신건가..?
+# muzi가 frodo 고르고 또 고르면 report에서 똑같이 나올테니까 set으로 중복 제거,, 
+for r in set(report):
+    reports[r.split()[1]] +=1
+
+for r in set(report):
+    # 신고 횟수가 k번 이상이라면 
+    if reports[r.split()[1]] >=k:
+        # r.split()[1]을 고른 신고자의 index를 찾아 
+        # answer의 인덱스에 넣고 +1 해주기 
+        answer[id_list.index(r.split()[0])] += 1
+    
+print(answer)
+
+# 02. 내 풀이랑 비슷한데 더 효율적인 방법
+
+
+
+answer = {x : 0 for x in id_list}
+dict = {x : [] for x in id_list}  # key (신고당한사람) : value (신고자) 
+
+for re in report:
+    key = re.split()[0]
+    val = re.split()[1]
+    
+    if key not in dict[val]:
+        dict[val] = dict[val]+[key]
+
+for id in id_list:
+    if len(dict[id]) >=k:
+        for i in dict[id]:
+            answer[i]+=1
+        
+list(answer.values())
